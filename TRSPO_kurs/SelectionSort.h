34f951125276f464f15bd1eb94c434d8b4dab784
Andrew Belov <fearess7@gmail.com>
2018-12-13 12:03:24 +0200

    Added selection sort

---
+++ TRSPO_kurs/SelectionSort.h
@@ -0,0 +1,25 @@
+#pragma once
+
+void swap(int &a, int &b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void selectionSort(int* A, int n) {
+
+	for (int j = 0; j < n - 1; j++) {
+		int smallest = j;
+
+		// tests all values after index j against the value at index j.
+		// If value at index i is less that value at index j/smallest, swap the
+		// two with each other
+		for (int i = j + 1; i < n; i++) {
+			if (A[i] < A[smallest]) {
+				smallest = i;
+			}
+		}
+
+		swap(A[j], A[smallest]);
+	}
+}
